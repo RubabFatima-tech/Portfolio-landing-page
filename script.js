c031bc7d7202de6c198590a000b23a58593c10e2
RubabFatima-tech <rubabfatima368@gmail.com>
2025-04-24 00:57:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script will show a welcome message in the console
+console.log("Welcome to Rubab Fatima's Landing Page!");
+
+// Optional: A simple alert when the page loads
+window.onload = function () {
+  alert("Thanks for visiting my portfolio!");
+};
